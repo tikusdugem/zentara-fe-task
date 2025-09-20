3cd412030e8d210fc09ca82f6b100a4a00ed2e9b
Nakya Santini <nakya.santini@gmail.com>
2025-09-20 21:02:40 +0700

    feat(analysis): slicing with recommendations

--- src/app/analysis/page.tsx
+++ src/app/analysis/page.tsx
@@ -1,14 +1,18 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
-import { Flex, message } from "antd";
+import { Card, Flex, message } from "antd";
 
 const PROXY_API_URL = process.env.NEXT_PUBLIC_PROXY_API_URL;
 
 const Analysis = () => {
   const [messageApi, contextHolder] = message.useMessage();
+
+  const [recommendations, setRecommendations] = useState<string>("");
 
   const searchParams = useSearchParams();
   const country = searchParams.get("country");
@@ -23,8 +27,9 @@ const Analysis = () => {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ country, threat, capital, continent }),
       });
+      const { content } = await res.json();
 
-      console.log(await res.json());
+      setRecommendations(content);
     } catch (error) {
       messageApi.open({
         type: "error",
@@ -45,7 +50,28 @@ const Analysis = () => {
 
       <Flex justify="center">
         <div style={{ width: "100%", maxWidth: 1440, padding: 16 }}>
-          Analysis
+          <Card title="Analysis" style={{ minWidth: 288 }}>
+            <ReactMarkdown
+              remarkPlugins={[remarkGfm]}
+              components={{
+                p: ({ children }) => (
+                  <p style={{ marginBottom: "1em" }}>{children}</p>
+                ),
+                ol: ({ children }) => (
+                  <ol style={{ marginBottom: "1em", marginLeft: "1em" }}>
+                    {children}
+                  </ol>
+                ),
+                ul: ({ children }) => (
+                  <ul style={{ marginBottom: "1em", marginLeft: "1em" }}>
+                    {children}
+                  </ul>
+                ),
+              }}
+            >
+              {recommendations}
+            </ReactMarkdown>
+          </Card>
         </div>
       </Flex>
     </>
